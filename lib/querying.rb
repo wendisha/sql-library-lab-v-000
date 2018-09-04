a8a0149d941b6a8ea0a263ac51dbe11a1622c6e9
Wendy Calderon <wendycalderon@gmail.com>
2018-09-04 02:44:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT title, year FROM books
-  JOIN "
+  JOIN series ON Books.series_id = Series.id 
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
