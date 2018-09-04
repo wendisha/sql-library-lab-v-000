a0ea42e30a92680c9dfd72623d7d472f7aaf2726
Wendy Calderon <wendycalderon@gmail.com>
2018-09-04 03:10:34 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,8 +6,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT name, motto FROM characters 
-  GROUP BY motto
-  HAVING MAX(Characters.motto)"
+  ORDER BY LENGHT(motto) DESC LIMIT(1)"
 end
 
 
