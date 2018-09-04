36785b86ea7a3214611558a35d9c804d69b65847
Wendy Calderon <wendycalderon@gmail.com>
2018-09-04 03:46:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT title FROM series 
   JOIN characters ON Series.id = Characters.series_id
-  GROUP BY species"
+  GROUP BY Series.title
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
